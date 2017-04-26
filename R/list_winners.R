cabe74f10aafa8629900aafd2deb3199f12f75f9
HajkD <hajkdrost@googlemail.com>
2017-04-26 22:58:11 +0100

    add error handling for winners

--- R/list_winners.R
+++ R/list_winners.R
@@ -1,7 +1,17 @@
 #' @title Winner Retrieval
 #' @description Retrieval of winner names.
+#' @param language programming language for which winners shall be retrieved.
 #' @export
-list_winners <- function() {
+list_winners <- function(language = "R") {
+  
+  if (!is.element(language, available_languages()))
+    stop("Do you also want to become a winner for the ",language," language? Or are you already a winner? No problem! Just write me an issue: https://github.com/HajkD/winnr/issues.")
+  
+  if (language == "R") {
+    
+    
+    
+  }
   
   
 }
